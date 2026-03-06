68ca4073bff8fb744a9b531b5b71cc34ea5eda1b
Said Obakrim <47177344+sobakrim@users.noreply.github.com>
2026-03-06 11:45:45 +0100

    Delete test_simulate_requires_jax function

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,7 +1,3 @@
 # tests/test_optional_jax.py
 import pytest
 from MLQuantile4SpaceTime.st_grf import simulate_gneiting_jax
-
-def test_simulate_requires_jax():
-    with pytest.raises(ImportError):
-        simulate_gneiting_jax(None, None, None, L=1)
